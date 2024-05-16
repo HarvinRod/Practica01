4530f51bccbd3fffe931621838f84ef6d97ad306
HarvinRod <165622692+HarvinRod@users.noreply.github.com>
2024-05-16 00:04:02 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+   include 'BD/conexion.php'
+
+?>
+<?php
+include("template/Header.php");
+include("template/Footer.php");
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <img src="img/Inicio.png" class="img-fluid rounded mx-auto d-block" style="max-height: 500px;" alt="Imagen Biblioteca">
+            <h1 class="display-4 mt-3 mb-4">Biblioteca de Harvin </h1>
+            <hr class="my-2">
+            <p></p>
+            <p class="lead">
+                    
+            </p>
+        </div>
+    </div>
+</div>
+
+<style>
+    .font-times {
+        font-family: 'Times New Roman', Times, serif;
+        font-size: 24px; 
+    }
+</style>
+</body>
+</html>
+
